5207363f3f647d666f910f753bead00f81497738
Meir Yust <meyu251@gmail.com>
2024-03-03 12:40:13 +0200

    add first file

---
+++ my_Knapsack.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int knapSack (int weights[], int values[] , int selected_bool[]){
+
+
+}
+
+
+int main(){
+
+    // char products[]
+    int values[5] = {0};
+    int weights[5] = {0};
+    int isSelect[5] = {0};
+    int i;
+
+    // @todo get products names into an array
+    printf("enter the values of the products:");
+    for(i=0; i<5; i++){
+        scanf("%d", &values[i]);
+    }
+    printf("enter the weights of the products:");
+    for(i=0; i<5; i++){
+        scanf("%d", &weights[i]);
+    }
+
+    
+
+
+    return 0;
+}
